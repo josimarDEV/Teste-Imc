f5282adc5fd70ae7648b3b2e124234bcee268c38
josimarDEV <josimar_504@hotmail.com>
2021-07-28 15:56:27 -0300

    Projeto Teste de IMC Bootcamper Santander_Dio

--- imcTeste/app/src/main/java/com/josimar/imcteste/MainActivity.kt
+++ imcTeste/app/src/main/java/com/josimar/imcteste/MainActivity.kt
@@ -2,6 +2,7 @@ package com.josimar.imcteste
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -24,30 +25,82 @@ class MainActivity : AppCompatActivity() {
         if (peso != null && altura != null) {
             val imc = peso / (altura * altura)
             val imcformatado = String.format("%.2f", imc)
-            when{
+
+            abaixo.visibility = View.GONE
+            abaixo2.visibility = View.GONE
+            ideal.visibility = View.GONE
+            ideal2.visibility = View.GONE
+            acima.visibility = View.GONE
+            acima2.visibility = View.GONE
+            grau1.visibility = View.GONE
+            grau1_1.visibility = View.GONE
+            grau2.visibility = View.GONE
+            grau2_1.visibility = View.GONE
+            grau3.visibility = View.GONE
+            grau3_1.visibility = View.GONE
+
+
+            abaixo3.visibility = View.VISIBLE
+            abaixo4.visibility = View.VISIBLE
+            ideal3.visibility = View.VISIBLE
+            ideal4.visibility = View.VISIBLE
+            acima3.visibility = View.VISIBLE
+            acima4.visibility = View.VISIBLE
+            grau1_2.visibility = View.VISIBLE
+            grau1_3.visibility = View.VISIBLE
+            grau2_2.visibility = View.VISIBLE
+            grau2_3.visibility = View.VISIBLE
+            grau3_2.visibility = View.VISIBLE
+            grau3_3.visibility = View.VISIBLE
+
+            when {
                 imc <= 18.5 -> {
-                    resultado.text = "Seu IMC é $imcformatado e está abaixo do peso"
+                    resultado.text = "IMC = $imcformatado"
                     resultado.setBackgroundColor(Color.rgb(14, 228, 203))
+                    abaixo.visibility = View.VISIBLE
+                    abaixo2.visibility = View.VISIBLE
+                    abaixo3.visibility = View.INVISIBLE
+                    abaixo4.visibility = View.INVISIBLE
                 }
                 imc in 18.6..24.9 -> {
-                    resultado.text = "Seu IMC é $imcformatado e está no peso ideal"
+                    resultado.text = "IMC = $imcformatado"
                     resultado.setBackgroundColor(Color.rgb(50, 244, 6))
+                    ideal.visibility = View.VISIBLE
+                    ideal2.visibility = View.VISIBLE
+                    ideal3.visibility = View.INVISIBLE
+                    ideal4.visibility = View.INVISIBLE
                 }
                 imc in 25.0..29.9 -> {
-                    resultado.text = "Seu IMC é $imcformatado e está levemente acima do peso"
+                    resultado.text = "IMC = $imcformatado"
                     resultado.setBackgroundColor(Color.rgb(255, 235, 59))
+                    acima.visibility = View.VISIBLE
+                    acima2.visibility = View.VISIBLE
+                    acima3.visibility = View.INVISIBLE
+                    acima4.visibility = View.INVISIBLE
                 }
                 imc in 30.0..34.9 -> {
-                    resultado.text = "Seu IMC é $imcformatado e está com obesidade grau 1"
+                    resultado.text = "IMC = $imcformatado"
                     resultado.setBackgroundColor(Color.rgb(255, 193, 7))
+                    grau1.visibility = View.VISIBLE
+                    grau1_1.visibility = View.VISIBLE
+                    grau1_2.visibility = View.INVISIBLE
+                    grau1_3.visibility = View.INVISIBLE
                 }
                 imc in 35.0..39.9 -> {
-                    resultado.text = "Seu IMC é $imcformatado e está com obesidade grau 2"
+                    resultado.text = "IMC = $imcformatado"
                     resultado.setBackgroundColor(Color.rgb(255, 152, 0))
+                    grau2.visibility = View.VISIBLE
+                    grau2_1.visibility = View.VISIBLE
+                    grau2_2.visibility = View.INVISIBLE
+                    grau2_3.visibility = View.INVISIBLE
                 }
-                imc  >= 40.0 -> {
-                    resultado.text = "Seu IMC é $imcformatado e está com obesidade grau 3"
+                else -> {
+                    resultado.text = "IMC = $imcformatado"
                     resultado.setBackgroundColor(Color.rgb(255, 87, 34))
+                    grau3.visibility = View.VISIBLE
+                    grau3_1.visibility = View.VISIBLE
+                    grau3_2.visibility = View.INVISIBLE
+                    grau3_3.visibility = View.INVISIBLE
                 }
             }
         }
